4b8cda753b5c35448a11395ab080ad762835fd4c
zhang-guangyi <1611875448@qq.com>
2023-11-03 11:29:32 +0800

    Second

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=3  python3 deepsc_main.py  --cr  3  --batch_size 64 \
---test_batch_size 50  --lr 0.0001  --output_dir  output_cr3_vitsc_rt_8dB --model ViTSC \
- --resume  ckpt_response/ckpt-t-cr3-vitsc-t8db-awgn.pth
+--test_batch_size 50  --lr 0.0001  --output_dir  output_cr3_vitsc_rt_18dB --model ViTSC \
+ --resume  ckpt/ckpt-t-cr3-vitsc-t18db-awgn.pth  --test
